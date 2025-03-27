0e47e086607ae35919008cf0440db49abc8989b0
msnc <msnc@g5c26.cin.ufpe.br>
2025-03-27 14:06:57 -0300

    atcoder 398 g left + graph

---
+++ Solutions/AtCoder/ABC398/G.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using i64 = long long;
+using u32 = unsigned;
+using u64 = unsigned long long;
+constexpr i64 inf = 1E18;
+constexpr int N = 2e5 + 5, MOD = 1e9 + 7;
+
+void solve() {
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int tt = 1; // cin >> tt;
+    while(tt--) {
+        solve();
+    }
+}
