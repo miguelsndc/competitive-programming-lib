99c6db755b633eb643c389888d7a5f806e676da9
miguelsndc <mglsndc@gmail.com>
2025-03-29 18:50:47 -0300

    vvvv

--- Solutions/Codeforces/Final/I.cpp
+++ Solutions/Codeforces/Final/I.cpp
@@ -1,7 +1,13 @@
-/*
-    Exponenciacao Rapida de Matrizes  O(m³ log (b))
-    Calcula recorrências lineares
-*/
+#include <bits/stdc++.h>
+using namespace std;
+
+using i64 = long long;
+using u32 = unsigned;
+using u64 = unsigned long long;
+constexpr i64 inf = 1E18;
+const int mod = 1e9 + 7;
+
+
 typedef vector<vector<i64>> matrix;
 
 matrix init(int size) {
@@ -11,17 +17,18 @@ matrix init(int size) {
 
 vector<i64> vecmul(matrix m, vector<i64> vec, i64 sz, i64 mod) {
     assert(vec.size() == sz);
-    vector<i64> ans;
+    vector<i64> ans(sz);
     for (int i = 0; i < sz; i++) {
-        for (int j = 0; j < sz; i++) {
+        for (int j = 0; j < sz; j++) {
             ans[i] += (vec[j] * (m[i][j]));
+            ans[i] %= mod;
         }
     }
     return ans;
 }
 
 matrix matmul(matrix m1, matrix m2, i64 mod, i64 sz) {
-    matrix ans;
+    matrix ans = init(sz);
     for (int i = 0; i < sz; i++) {
         for (int j = 0; j < sz; j++) {
             for (int k = 0; k < sz; k++) {
@@ -34,7 +41,7 @@ matrix matmul(matrix m1, matrix m2, i64 mod, i64 sz) {
 
 // O(log(b))
 matrix fexp(matrix p, i64 b, i64 mod, i64 sz) {
-    matrix ans;
+    matrix ans = init(sz);
     for(int i = 0; i < sz; i++) ans[i][i] = 1;
     while(b){
         if(b & 1) ans = matmul(ans, p, mod, sz);
@@ -42,4 +49,25 @@ matrix fexp(matrix p, i64 b, i64 mod, i64 sz) {
         b >>= 1;
     }
     return ans;
+}
+
+int main() 
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    i64 n; cin >> n;
+    int x, y; cin >> x >> y;
+    matrix m = {{0, 1}, {1, 1}};
+    matrix ans = fexp(m, max(n - 2, 0LL), mod, 2);
+    vector<i64> v = vecmul(ans, {x, y}, 2, mod);
+    if (n & 1) {
+        cout << "Samuell\n";
+    } else {
+        cout << "Lleumas\n";
+    }
+    if (n <= 2) {
+        cout << v[n - 1] % mod;
+    } else {
+        cout << v[1] % mod << '\n';
+    }
 }
