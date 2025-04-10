14675c56dcd4eca1bf0879ab1892f88534f6e9f6
miguelsndc <mglsndc@gmail.com>
2025-04-10 12:03:48 -0300

    few

---
+++ Solutions/Codeforces/MIscellaneous/test.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+
+using i64 = long long;
+using u32 = unsigned;
+using u64 = unsigned long long;
+
+constexpr i64 inf = 1E18;
+constexpr int mod = 1e9 + 7, maxn = 1e5 + 5;
+
+void solve() {
+    int a, b; cin >> a >> b;
+}
+
+int main() 
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int tt = 1; // cin >> tt;
+    while(tt--) {
+        solve();
+    }
+}
