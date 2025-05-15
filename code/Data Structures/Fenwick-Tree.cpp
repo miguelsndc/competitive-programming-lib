1d0dcd5257405b26d0fa10456eaf390e07d2d4d1
miguelsndc <mglsndc@gmail.com>
2025-05-15 12:58:26 -0300

    day1 of clearing range queries cses

--- code/Data Structures/Fenwick-Tree.cpp
+++ code/Data Structures/Fenwick-Tree.cpp
@@ -2,22 +2,23 @@
     Fenwick Tree - Range Queries
 */
 
-template<typename T = int>
+template <typename T = int>
 struct FenwickTree {
     vector<T> bit(maxn), arr(maxn);
 
-	// O(log(n))
+    // O(log(n))
     void add(int pos, int val) {
         for (int i = pos + 1; i < maxn; i += (i & (-i))) bit[i] += val;
     }
 
-	// O(log(n))
+    // O(log(n))
     void pset(int pos, int val) {
-        int delta = val - arr[pos]; arr[pos] = val; 
+        int delta = val - arr[pos];
+        arr[pos] = val;
         add(pos, delta);
     }
 
-	// O(log(n))
+    // O(log(n))
     T query(int pos) {
         T sum = 0;
         for (int i = pos + 1; i > 0; i -= (i & (-i))) sum += bit[i];
