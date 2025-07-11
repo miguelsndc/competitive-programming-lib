8f89dc0c4c62212344a765eabc0c915333a47c75
miguelsndc <mglsndc@gmail.com>
2025-07-11 10:49:45 -0300

    dsa

--- Solutions/UVa/UVA12577.cpp
+++ Solutions/UVa/UVA12577.cpp
@@ -1,15 +1,8 @@
-minimizar a maior soma de 2 vacas
+aabbbbbzaaad
 
-busca binaria consigo leitar ? todo mundo <= x unidades de tempo? se sim ent consigo
-com x+1, x+2, etc
+enquanto o prefixo tiver bom 
 
-aí é só checar.
+(menor, 2omenor, 3omenor, etc..) skipa
+ai vai ter algo tipo
 
-pra um tempo fixo t
-
-parea as vacas com soma <= x
-sempre existe um pareamento pq m é par
-
-pra uma vaca com tempo s, procura o maior cara <= (t - s) 
-pq se essa vaca pegar alguem menor,s, s+ 1, entao o cara maior vai ter q ser pareado com alguem maior
-o q vai aumentar o tempo 
+(nesimo menor, (n-i) menor), posso trocar dois caracteres e mandar alguem pra longe
