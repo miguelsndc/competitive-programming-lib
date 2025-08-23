e9a950106bc63aaf79c4dbd74df7dbe3831c2abd
miguelsndc <mglsndc@gmail.com>
2025-08-23 00:29:27 -0300

    dsadsmo

--- Solutions/UVa/100.cpp
+++ Solutions/UVa/100.cpp
@@ -1,52 +1,54 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-using ll = long long;
-
-const int maxn = 1e6 + 5;
-vector<int> cycle_length(maxn);
-
-void precalc() {
-    for (int i = 1; i < maxn; i++) {
-        int cnt = 0;
-        ll n = 1LL * i;
-        while (1) {
-            cnt++;
-            if (n == 1) break;
-            if (n & 1)
-                n = 3 * n + 1;
-            else
-                n /= 2;
+#define ll long long
+#define all(x) (x).begin(), (x).end() 
+class SparseTable {
+    private:
+        int n, k;
+        vector<vector<int>> st;
+    public: 
+    SparseTable(const vector<int> & v) {
+        n = v.size(); k = 31 - __builtin_clz(n) + 1;
+        st.resize(k); st[0] = v;
+        for (int i = 1; i < k; i++) {
+            st[i].resize(n - (1 << i) + 1);
+            for (int j = 0; j + (1 << i) <= n; j++) 
+                st[i][j] = max(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
         }
-        cycle_length[i] = cnt;
+    }
+    int query(int l, int r) {
+        int p = 31 - __builtin_clz(r - l + 1);
+        return max(st[p][l], st[p][r - (1 << p) + 1]);
     }
 };
-
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    precalc();
-
-    vector<vector<int>> st;
-
-    int n = maxn;
-    int k = 31 - __builtin_clz(n) + 1;
-    st.resize(k);
-    st[0] = cycle_length;
-    for (int i = 1; i < k; i++) {
-        st[i].resize(n - (1 << i) + 1);
-        for (int j = 0; j + (1 << i) <= n; j++) {
-            st[i][j] = max(st[i - 1][j], st[i - 1][j + (1 << (i - 1))]);
+void solve() {
+    const int mxn = 1e6 + 5;
+    vector<int> dp(mxn);
+    dp[1] = 1;
+    for (ll i = 2; i < mxn; i++) {
+        ll k = i;
+        int cnt = 1;
+        while(1) {
+            if (k & 1) k = 3 * k + 1;
+            else k /= 2;
+            cnt++;
+            if (k == 1) {
+                dp[i] = cnt;
+                break;
+            }
         }
     }
-
-    int x, y;
-    while ((cin >> x >> y) and x and y) {
-        auto [l, r] = minmax(x, y);
-        int p = 31 - __builtin_clz(r - l + 1);
-        cout << x << ' ' << y << ' ' << max(st[p][l], st[p][r - (1 << p) + 1]) << '\n';
+    SparseTable st(dp);
+    int i, j;
+    while((cin >> i >> j)) {
+        auto [l, r] = minmax(i, j);
+        int ans = st.query(l, r);
+        cout << i << ' ' << j << ' ' << ans << '\n';
     }
-
-    return 0;
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T = 1; // cin >> T;
+    while(T--) solve();
 }
